c8f0ee1f7bd675154439e219c31e27996c365cbc
user name <you@example.com>
2024-08-10 11:47:54 +0300

    sec commit

--- index.js
+++ index.js
@@ -6,9 +6,37 @@ import { bootstrap } from "./src/modules/bootstrap.js";
 import { AppError } from "./src/utilities/appError.js";
 import errorHandler from "./src/middleware/globalErrorHandling.js";
 import cors from "cors";
+import { catchError } from "./src/middleware/catchError.js";
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51PltAMJs7opwPS1443BNZCwPJgwtSJUf3eC63eyKV3sY1wHIwCXvd7uK5COGR0nYvz65hJpUndUIsODtD09f8A8D00YQAv6pXj"
+);
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
+
+//Before the express.json body parser
+app.post(
+  "/api/webhook",
+  express.raw({ type: "application/json" }),
+  catchError(async (req, res, next) => {
+    const sig = req.headers["stripe-signature"].toString();
+
+    let event = stripe.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.WEBHOOK - SIGNING - SECRET
+    );
+    let checkout;
+    if (event.type == "checkout.session.completed") {
+      checkout = event.data.object;
+    }
+
+    // Return a 200 res to acknowledge receipt of the event
+    res.json({ message: "success", checkout });
+  })
+);
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
